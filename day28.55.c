5cc31632dc1979fa5c97d92275cffb289affee0c
mhkrwt-1 <mahakwork1305@gmail.com>
2025-11-11 00:39:52 +0530

    Add files via upload

--- day28.55.c
+++ day28.55.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 int main()
 {
-    int i,j,n;
-    printf("Enter a value for n:");
-    scanf("%d",&n);
+    int n, i, j, count;
+    printf("Enter the value of n:");
+    scanf("%d", &n);
     for(i=2;i<=n;i++)
     {
-        int isPrime=1;
-        for(j=2;j*j<=i;j++)
+        count=0;
+        for(j=1;j<=i;j++)
         {
             if(i%j==0)
-            {
-                isPrime=0;
-                break;
-            }
+                count++;
         }
-        if(isPrime)
+        if(count==2)
+        {
             printf("%d ",i);
+        }
     }
 }
