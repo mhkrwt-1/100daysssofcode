871631566d7739f489e3bb652e3b641c37fca1fb
mhkrwt-1 <mahakwork1305@gmail.com>
2025-11-11 00:53:53 +0530

    Add files via upload

---
+++ day29.58.c
@@ -0,0 +1,19 @@
+//Q58: Find the maximum and minimum element in an array.
+#include <stdio.h>
+int main()
+{
+    int n,i,a[100],max,min;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    max=a[0];
+    min=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>max)
+            max=a[i];
+        if(a[i]<min)
+            min=a[i];
+    }
+    printf("Max=%d, Min=%d",max,min);
+}
