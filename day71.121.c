801574bf4fa0463452cfedc770392a34bfc1e043
mhkrwt-1 <mahakwork1305@gmail.com>
2025-11-25 23:28:49 +0530

    Add files via upload

--- day71.121.c
+++ day71.121.c
@@ -1,13 +1,4 @@
 //Q121: Write a C program that creates a text file named info.txt in write mode. The program should take the user’s name and age as input, and write them to the file using fprintf(). After writing, display a message confirming that the data was successfully saved.
-
-/*
-Sample Test Cases:
-Input 1:
-Name: Rahul, Age: 23
-Output 1:
-File created successfully! Data written to info.txt
-
-*/
 #include <stdio.h>
 
 int main() 
